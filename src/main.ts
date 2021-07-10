3c67cc6a2d116f1ef83bff6bd69ba2e3e9d1a1ed
Wilson Lin <code@wilsonl.in>
2021-07-10 17:24:44 +1000

    Improve performance and usability

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
 import derivedComparator from "@xtjs/lib/js/derivedComparator";
-import multiComparator from "@xtjs/lib/js/multiComparator";
-import reversedComparator from "@xtjs/lib/js/reversedComparator";
-import propertyComparator from "@xtjs/lib/js/propertyComparator";
-import assertExists from "@xtjs/lib/js/assertExists";
 import Dict from "@xtjs/lib/js/Dict";
+import lazyMap from "@xtjs/lib/js/lazyMap";
+import multiComparator from "@xtjs/lib/js/multiComparator";
+import propertyComparator from "@xtjs/lib/js/propertyComparator";
+import reversedComparator from "@xtjs/lib/js/reversedComparator";
+
+type ParamChild = {
+  name: string;
+  parser: (raw: string) => any | undefined;
+  prefix: string;
+  subroute: Route<any, any>;
+};
 
 export class Route<Value, Parsed extends { [name: string]: any }> {
   private value?: (parsed: Parsed) => Value;
-  private readonly childrenByPrefix = new Dict<
-    string,
-    Array<{
-      // If name/parser is not defined, this is a literal path component, and the prefix must match the full component.
-      name?: string;
-      parser?: (raw: string) => any | undefined;
-      subroute: Route<any, any>;
-    }>
-  >();
+  private readonly paramChildren = new Array<ParamChild>();
+  // this.paramChildren can only change by adding elements, so comparing length is safe and fast.
+  private readonly lazyParamChildrenMap = lazyMap<ParamChild[], ParamChild[]>(
+    (a, b) => a.length === b.length
+  );
+  private readonly literalChildren = new Dict<string, Route<any, any>>();
 
   private constructor() {}
 
@@ -23,31 +27,38 @@ export class Route<Value, Parsed extends { [name: string]: any }> {
     return new Route();
   }
 
-  private addChild(
+  private addParamChild(
     prefix: string,
-    name?: string,
-    parser?: (raw: string) => any | undefined
+    name: string,
+    parser: (raw: string) => any | undefined
   ): any {
+    // Find any existing exact matches so that users can also do something like `route.param("user", user).end(...); route.param("user", user).lit("edit").end(...);`
+    // instead of having to do `const userRoute = route.param("user", user); userRoute.end(...); userRoute.lit("edit").end(...);`.
+    const existing = this.paramChildren.find(
+      (c) => c.name === name && c.prefix === prefix && c.parser === c.parser
+    );
+    if (existing) {
+      return existing.subroute;
+    }
     const subroute = new Route();
-    this.childrenByPrefix
-      .computeIfAbsent(prefix, () => [])
-      .push({
-        name,
-        parser,
-        subroute,
-      });
+    this.paramChildren.push({
+      name,
+      parser,
+      prefix,
+      subroute,
+    });
     return subroute as any;
   }
 
   lit(name: string): Route<Value, Parsed> {
-    return this.addChild(name);
+    return this.literalChildren.computeIfAbsent(name, () => new Route());
   }
 
   param<N extends string, T>(
     name: N,
     parser: (raw: string) => T | undefined
   ): Route<Value, Parsed & { [name in N]: T }> {
-    return this.addChild("", name, parser);
+    return this.addParamChild("", name, parser);
   }
 
   prefixedParam<N extends string, T>(
@@ -55,10 +66,13 @@ export class Route<Value, Parsed extends { [name: string]: any }> {
     prefix: string,
     parser: (raw: string) => T | undefined
   ): Route<Value, Parsed & { [name in N]: T }> {
-    return this.addChild(prefix, name, parser);
+    return this.addParamChild(prefix, name, parser);
   }
 
   end(valFn: (parsed: Parsed) => Value): void {
+    if (this.value) {
+      throw new ReferenceError(`End already set`);
+    }
     this.value = valFn;
   }
 
@@ -68,34 +82,39 @@ export class Route<Value, Parsed extends { [name: string]: any }> {
       // We have reached end of path.
       return this.value?.(parsed);
     }
-    for (const [prefix, children] of [...this.childrenByPrefix].sort(
-      multiComparator(
-        reversedComparator(derivedComparator((e) => e[0].length)),
-        propertyComparator(0)
-      )
+    const litChild = this.literalChildren.get(comp);
+    if (litChild) {
+      return litChild._parse(parsed, components);
+    }
+
+    for (const {
+      name,
+      parser,
+      prefix,
+      subroute,
+    } of this.lazyParamChildrenMap.map(this.paramChildren, (children) =>
+      children
+        .slice()
+        .sort(
+          multiComparator(
+            reversedComparator(derivedComparator((e) => e.prefix.length)),
+            propertyComparator("name")
+          )
+        )
     )) {
       if (!comp.startsWith(prefix)) {
         continue;
       }
       const raw = comp.slice(prefix.length);
 
-      for (const child of children) {
-        if (child.name != undefined) {
-          // This is a parameterised component.
-          const v = assertExists(child.parser)(raw);
-          if (v !== undefined) {
-            parsed[child.name] = v;
-            return child.subroute._parse(parsed, components);
-          }
-        } else {
-          // This is a literal component.
-          if (!raw) {
-            // Matched entire component.
-            return child.subroute._parse(parsed, components);
-          }
-        }
+      // This is a parameterised component.
+      const v = parser(raw);
+      if (v !== undefined) {
+        parsed[name] = v;
+        return subroute._parse(parsed, components);
       }
     }
+
     // If we've reached here, we couldn't find any matching route.
     return undefined;
   }
